af3071c67d9c2746225aced154f09e012dbe09ec
eselbeus <eselbeus@gmail.com>
2019-01-31 20:52:24 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -32,7 +32,7 @@ class Owner
     @pets = {:fishes => [], :dogs => [], :cats => []}
   end
   
-  def buy_fish(Fish.name)
+  def buy_fish(name)
     @pets[:fishes] << Fish.name
     self.pets[:fishes]
     Fish.name
