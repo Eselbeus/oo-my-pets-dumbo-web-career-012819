797e855b6ba8784db61a81c023d4ffbaef2e43f1
eselbeus <eselbeus@gmail.com>
2019-01-31 21:10:17 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -33,11 +33,13 @@ class Owner
   def buy_fish(name)
     @pets[:fishes] << Fish.new(name)
   end
+  ]
   def buy_cat(name)
     @pets[:cat] << Cat.new(name)
   end
+  
   def buy_dog(name)
-    @pets[:dog] << Fish.new(dog)
+    @pets[:dog] << Dog.new(name)
   end
   
   
